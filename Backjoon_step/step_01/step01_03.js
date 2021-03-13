8c00a97f9c5af2f08e0e42008b5a3e3573125225
GunHee Choi <godrjsmgl@gmail.com>
2021-03-13 13:51:43 +0000

    Solve Backjoon Step01 01_03

---
+++ Backjoon_step/step_01/step01_03.js
@@ -0,0 +1,20 @@
+/**
+* 2021. 03. 13
+* Creater : Gunhee Choi
+* Problem Number : 	10171
+* Title : 고양이
+* Problem :
+	아래 예제와 같이 고양이를 출력하시오.
+* Input : None
+* Output :
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ 
+**/
+
+console.log("\\    /\\");
+console.log(" )  ( \')");
+console.log("(  /  )");
+console.log(" \\(__)|");
