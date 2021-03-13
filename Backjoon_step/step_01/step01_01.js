3ae80f4a3a8a48ba8c528927c4a3d25fd81309de
GunHee Choi <godrjsmgl@gmail.com>
2021-03-13 13:48:37 +0000

    Add Day in step01_01, step01_02

--- Backjoon_step/step_01/step01_01.js
+++ Backjoon_step/step_01/step01_01.js
@@ -1,1 +1,13 @@
+/**
+* 2021. 03. 12
+* Creater : Gunhee Choi
+* Problem Number : 2557
+* Title : Hello World
+* Problem :
+	Hello World!를 출력하시오.
+* Input : None
+* Output : Hello World!
+**/
+
+
 console.log('Hello World!');
